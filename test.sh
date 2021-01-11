d40e7f0b6e4eb2ca7e3cd1e43d52c51893443e02
Abdelbari Allali <abdelbari.allali@c02dk4ewpn5v.wren.wrenkitchens.com>
2021-01-11 15:01:59 +0000

    working 8RAM and 2CPUs requirement " all three docker services run on vagrant up

--- test.sh
+++ test.sh
@@ -1,3 +1,42 @@
 #!/bin/bash
 
+#update package manager 
 yum update -y 
+
+echo "Beginning Docker install"
+
+# Install Docker-ce
+yum install docker -y
+
+# Start Docker
+
+systemctl enable docker
+
+systemctl start docker
+
+echo "Docker successfully installed"
+echo "Beginning post installation steps"
+
+# Post Installation Steps
+
+# Create Docker group
+groupadd docker
+
+# Add user to the docker group
+usermod -aG docker $USER
+
+echo "Creating RunDeck service"
+
+# create runddeck, jenkins and prometheus containers
+docker run -d --name some-rundeck -p 4440:4440 -v data:/home/rundeck/server/data rundeck/rundeck:3.3.7
+
+echo "Creating Jenkins service"
+
+docker run -d -p 8080:8080 -p 50000:50000 jenkins/jenkins:lts
+
+echo "Creating Prometheus service"
+
+docker run -d -p 9090:9090 prom/prometheus
+
+
+
